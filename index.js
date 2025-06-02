05bbe6c3b565690adc7fce1ee6a68da597e0549d
SergioCaMi <sergiocami84@gmail.com>
2025-06-02 18:33:43 +0200

    Implement word filtering endpoint and update package dependencies

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
+const _ = require("lodash");
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express(); //  Crear instancia de Express
+const PORT = process.env.PORT || 3000; // Puerto del servidor
 
 // Cargar palabras desde archivo
 const words = require("./data/words.json");
@@ -12,17 +13,30 @@ const words = require("./data/words.json");
 // Middleware
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 
 // Endpoints
 
 // Iteración 1
+app.get("/api/v1/words", (req, res) => {
+  if (req.query.length) {
+    const lengthWords = req.query.length;
+    const wordResult = words.filter((w) => w.length == +lengthWords);
+    if (wordResult.length > 0) {
+      res.json({ word: _.sample(wordResult) });
+    } else {
+      res.status(404).send("No existen palabras con esa condición.");
+    }
+  } else {
+    res.json({ word: _.sample(words) });
+  }
+});
 
 // Iteración 2
 
 // Iteración 3
 
 // Iteración 4
-
 
 // 404 para rutas no existentes
 app.use((req, res) => {
